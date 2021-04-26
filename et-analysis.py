090a4f27d643b4ebec2d5ba3496ab653b1b41500
ajoshufish <joshu.fisher@gmail.com>
2021-04-26 10:50:10 -0400

    update json loc

---
+++ et-analysis.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import gspread
+
+st.title('Earth Treks Occupancy Analysis')
+
+@st.cache(allow_output_mutation=True)
+def load_data():
+    gc = gspread.service_account(filename='/Users/joshu/Documents/python projs/ET_Analysis/etanalysis/etetdataaccess.json')
+    ws = gc.open_by_key("1pAzzV_ywejUJKWkkrPSdHfssMp3qcCEbmyK6XiZfxTQ").worksheet('Data')
+    return ws.get_all_records()
+
+
+st.text('Loading data...')
+data = load_data()
+headers = data.pop(0)
+df = pd.DataFrame(data, columns=headers)
+st.text('Loading data...done!')
+
+col1 = df['Time']
+col2 = df['Columbia Occupancy']
+combo = [col1,col2]
+plots = pd.DataFrame(combo)
+plot_t = plots.T
+
+st.line_chart(plot_t)
+st.write(plot_t)
