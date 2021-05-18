cbf4a96eb981b1282c89aa05f233302e3ca77e3e
Joshu Fisher <joshu.fisher@gmail.com>
2021-05-18 15:05:33 -0400

    Trimming dead space in data recording

--- et-analysis.py
+++ et-analysis.py
@@ -74,14 +74,65 @@ if(len(filtDays) > 0):
 filt = filt[filt.CTime >= time1]
 filt = filt[filt.CTime <= time2]
 
+gymTimeHead = ['Gym', 'WeekOpen', 'WeekClose', 'SatOpen', 'SatClose', 'SunOpen', 'SunClose']
+timTime = ['Timonium', time(16,00), time(22,00), time(9,00), time(16,00), time(9,00), time(16,00)]
+coloTime = ['Columbia', time(6,00), time(23,00), time(8,00), time(20,00), time(8,00), time(18,00)]
+rocTime = ['Rockville', time(6,00), time(23,00), time(8,00), time(20,00), time(8,00), time(18,00)]
+hamTime = ['Hampden', time(6,00), time(22,00), time(8,00), time(20,00), time(8,00), time(18,00)]
+cryTime = ['Crystal City', time(6,00), time(23,00), time(8,00), time(20,00), time(8,00), time(18,00)]
+gymTime = pd.DataFrame(list(zip(timTime, coloTime, rocTime, hamTime, cryTime))).T
+gymTime.columns = gymTimeHead
+
+#programmatically find days we didn't collect data so we can filter them out on a plotting level
+dt_all = pd.date_range(start=filt['DateTime'].iloc[0], end=filt['DateTime'].iloc[-1], freq='H')
+dt_obs = [d.strftime('%Y-%m-%d') for d in filt['DateTime']]
+dt_breaks = [d for d in dt_all.strftime('%Y-%m-%d').tolist() if not d in dt_obs]
+
+dt_breaks = list(dict.fromkeys(dt_breaks))
+
+#track down times when the gym isn't open and throw those out of the plot? not working
+# dt_late = [d for d in dt_all if (d.time() > gymTime.loc[gymTime.Gym == gym, 'WeekClose'][1]) 
+#     and (d.weekday() < 5)
+#     | (d.time() > gymTime.loc[gymTime.Gym == gym, 'SatClose'][1]) 
+#         and (d.weekday() == 5)
+# ]
+# dt_late =[]
+# for d in dt_all:
+#     if (d.time() > gymTime.loc[gymTime.Gym == gym, 'WeekClose'][1]) and (d.weekday() < 5):
+#         dt_late.append(d)
+#     elif (d.time() > gymTime.loc[gymTime.Gym == gym, 'SatClose'][1]) and (d.weekday() == 5):
+#         dt_late.append(d)
+#     elif (d.time() > gymTime.loc[gymTime.Gym == gym, 'SunClose'][1]) and (d.weekday() == 6):
+#         dt_late.append(d)
+
+# dt_early =[]
+# for d in dt_all:
+#     if (d.time() < gymTime.loc[gymTime.Gym == gym, 'WeekOpen'][1]) and (d.weekday() < 5):
+#         dt_early.append(d)
+#     elif (d.time() < gymTime.loc[gymTime.Gym == gym, 'SatOpen'][1]) and (d.weekday() == 5):
+#         dt_early.append(d)
+#     elif (d.time() < gymTime.loc[gymTime.Gym == gym, 'SunOpen'][1]) and (d.weekday() == 6):
+#         dt_early.append(d)
+
+# dt_breaks.extend(dt_early)
+# dt_breaks.extend(dt_late)
+
 #plot it
 st.text("What's been going on at "+ gym+ ' ?')
 colorhelp = 'rgba(0,0,0,0)'
 fig = go.Figure()
-fig.add_trace(go.Scatter(name='', y=filt[gymOcc], x=filt['DateTime'], mode='markers+lines', hovertemplate='Occupancy: %{y} at %{text}', text=filt['Time'] + ' on '+ filt['ShDay'] +', ' + filt['Date']))
-fig.add_trace(go.Scatter(name='', y=filt[gymCap], x=filt['DateTime'], hovertemplate='Capacity: %{y} (%{text})', text=filt['Date']))
-fig.update_layout(xaxis_title='Date', yaxis_title='Occupancy', yaxis=dict(showspikes=True, spikemode = 'marker+toaxis', spikesnap = 'cursor'), xaxis=dict(type = "category", showspikes=True, spikemode = 'marker+toaxis', spikesnap = 'cursor'), margin_l=10, margin_r=10, margin_t=10, margin_b=10, hovermode='closest', showlegend=False)
-fig.update_xaxes(rangeslider_visible=True, tickvals=['one', 'two'], ticktext=[filt['DateTime'].dt.strftime('%m%d')])
+fig.add_trace(go.Scatter(name='', y=filt[gymOcc], x=filt['DateTime'], mode='markers+lines', 
+    hovertemplate='Occupancy: %{y} at %{text}', text=filt['Time'] + ' on '+ filt['ShDay'] +', ' + filt['Date']))
+
+fig.add_trace(go.Scatter(name='', y=filt[gymCap], x=filt['DateTime'], hovertemplate='Capacity: %{y} (%{text})', 
+    text=filt['Date']))
+
+fig.update_layout(xaxis_title='Date', yaxis_title='Occupancy',
+    yaxis=dict(showspikes=True, spikemode = 'marker+toaxis', spikesnap = 'cursor'), 
+    xaxis=dict(showspikes=True, spikemode = 'marker+toaxis', spikesnap = 'cursor'), 
+    margin_l=10, margin_r=10, margin_t=10, margin_b=10, hovermode='closest', showlegend=False)
+
+fig.update_xaxes(rangeslider_visible=True, rangebreaks=[dict(values=dt_breaks)])
 st.plotly_chart(fig , use_container_width=True)
 
 #debug/raw data
