9e48a3540171b9c65e57c28a7d035bc74040838a
Joshu Fisher <joshu.fisher@gmail.com>
2021-05-24 22:25:22 -0400

    txt sidestep

--- bughunt.py
+++ bughunt.py
@@ -14,3 +14,11 @@ defaultMax = time(20,00)
 # defaultMax = 20
 st.slider('Start of time range?', min_value=minTime, max_value=maxTime, value=defaultMin)
 st.slider('End of time range?', min_value=minTime, max_value=maxTime, value=defaultMax)
+
+times=['6:00 AM', '7:00 AM', '8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '1:00 PM', '3:00 PM', '4:00 PM', '5:00 PM', '6:00 PM', '7:00 PM', '8:00 PM', '9:00 PM', '10:00 PM', '11:00 PM']
+time1, time2 = st.select_slider('Choose range', options=times, value=('7:00 AM', '1:00 PM'))
+st.write(time1)
+st.write(time2)
+st.write(dt.strptime(time1, '%I:%M %p').time())
+st.write(dt.strptime(time2, '%I:%M %p').time())
+
