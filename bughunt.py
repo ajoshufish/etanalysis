774e7a696eb163287c688f74c2e30f60710ad889
Joshu Fisher <joshu.fisher@gmail.com>
2021-05-24 21:32:18 -0400

    tz aware min

--- bughunt.py
+++ bughunt.py
@@ -1,7 +1,21 @@
 import streamlit as st
 from datetime import time
+from datetime import datetime as dt
+import pytz
+
+eastern = pytz.timezone("US/Eastern")
+
+base = dt.strptime('6:00', '%H:%M')
+
+
+est = eastern.localize(base)
+
+
+conv1 = dt.time(est)
+st.write(conv1)
+
 minTime = time(6,00)
 maxTime = time(23,00)
 defaultMin = time(10,00)
 defaultMax = time(20,00)
-st.slider('Time of day?', min_value=minTime, max_value=maxTime, value=(defaultMin, defaultMax), format="LT")
+st.slider('Time of day?', min_value=conv1, max_value=maxTime, value=(defaultMin, defaultMax), format="LT")
